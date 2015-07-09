8ed66e573f2caf1007dbf4c8a98bef897c9ed8ed
leonardo_guto <leolleo.comp@gmail.com>
2015-07-09 15:00:47 -0300

    repasse da amostra e analise de florescencia

--- Cake/src/Controller/AmostrarepasseController.php
+++ Cake/src/Controller/AmostrarepasseController.php
@@ -11,6 +11,57 @@ class AmostrarepasseController extends AppController
 {
 
     /**
+    * List add
+    * 
+    * @param controle_id 
+    * @author Leonardo Cavalcante do Prado, Angelo Gustavo, Gabriel Rafal
+    * @return void or, if a post method is called, the redirect action
+    */
+    public function list_add( $controle_id = null ) 
+    {
+        $query = $this->Amostrarepasse->find('all')->where( ['fk_controle' => $controle_id] );
+        $this->paginate = [ 'maxLimit' => 5 ];
+        $this->set('amostras', $this->paginate($query));
+
+        $amostrarepasse = $this->Amostrarepasse->newEntity();
+
+        if ($this->request->is('post')) {
+            $amostrarepasse = $this->Amostrarepasse->patchEntity($amostrarepasse, $this->request->data);
+            
+            $amostrarepasse->set(['fk_controle' => $controle_id]);
+            
+            if ($this->Amostrarepasse->save($amostrarepasse)) {
+                $this->Flash->success('O repasse da amostra foi salvo.');
+                return $this->redirect(['action' => 'list_add', $controle_id]);
+            } else {
+                $this->Flash->error('O repasse da amostra não pôde ser salvo, por favor, tente novamente.');
+            }
+        }
+        $this->set(compact('amostrarepasse'));
+        $this->set('_serialize', ['amostrarepasse']);
+    }
+
+
+    /**
+    * DeletNoReturn method
+    * 
+    * @param  id, controle_id 
+    * @author Leonardo Cavalcante do Prado, Angelo Gustavo e Gabriel Rafael
+    * @return the redirect action
+    */
+    public function deleteNoReturn($id = null, $controle_id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $amostrarepasse = $this->Amostrarepasse->get($id);
+        if ($this->Amostrarepasse->delete($amostrarepasse)) {
+            $this->Flash->success('O repasse da amostra foi deletado.');
+        } else {
+            $this->Flash->error('O repasse da amostra não pôde ser deletado, por favor tente novamente.');
+        }
+        return $this->redirect( ['action'=> 'list_add', $controle_id] );
+    }
+
+    /**
      * Index method
      *
      * @return void
